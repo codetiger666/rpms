660d90e3afa601ab9e1781ca9b63a5abbc24d7ef
codetiger <codetiger806@gmail.com>
2026-08-19 09:19:56 +0800

    fix: adjust scripts

--- onedrive/onedrive-watch-dog.sh
+++ onedrive/onedrive-watch-dog.sh
@@ -4,8 +4,8 @@
 PROGRAM_PATH=$(dirname "$(realpath "$0")")
 
 if pgrep -f 'onedrive --confdir' > /dev/null; then
-    echo "当前进程存活" > ${PROGRAM_PATH}/watch.log
+    echo "$(date "+%Y-%m-%d %H:%M:%S") 当前进程存活" >> ${PROGRAM_PATH}/watch.log
 else
-    echo "当前进程未存活，重新拉起" > ${PROGRAM_PATH}/watch.log
+    echo "$(date "+%Y-%m-%d %H:%M:%S") 当前进程未存活，重新拉起" >> ${PROGRAM_PATH}/watch.log
     systemctl restart onedrive
 fi
